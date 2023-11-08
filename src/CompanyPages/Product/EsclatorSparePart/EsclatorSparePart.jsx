e236f08e2beb671721ff1a86c7fb7c74b6d2dd01
Abhishek Tiwari <a.tiwari@digicust.com>
2023-11-09 01:53:26 +0530

    commit

--- src/CompanyPages/Product/EsclatorSparePart/EsclatorSparePart.jsx
+++ src/CompanyPages/Product/EsclatorSparePart/EsclatorSparePart.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import  '../Lift/lift.css'
+import  '../Lift/Lift.css'
 
 import lift1 from '../../../assets/img/lift1.jpg'
 import lift4 from '../../../assets/img/lift4.jpg'
