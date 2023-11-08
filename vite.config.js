ddfb93d5bd2d7729ac57b4aa56c448b0eb2b767f
Abhishek Tiwari <a.tiwari@digicust.com>
2023-11-09 02:23:53 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build:{
+    outDir:"build"
+  },
   resolve: {
-    alias: {
-      './runtimeConfig': './runtimeConfig.browser', // <-- Fix from above
-    },
+    alias: [
+      {
+        find: './runtimeConfig',
+        replacement: './runtimeConfig.browser',
+      },
+    ]
   }
 })
