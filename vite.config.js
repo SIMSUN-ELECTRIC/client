111fbde0d01acfb93bda4a0ea5efc6abb7fe149a
Ajay-Goswami <goswamiajay2002@gmail.com>
2025-04-15 15:56:47 +0530

    change outDir build to dist

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build:{
-    outDir:"build"
+  build: {
+    outDir: "dist",
   },
   resolve: {
     alias: [
       {
-        find: './runtimeConfig',
-        replacement: './runtimeConfig.browser',
+        find: "./runtimeConfig",
+        replacement: "./runtimeConfig.browser",
       },
-    ]
-  }
-})
+    ],
+  },
+});
